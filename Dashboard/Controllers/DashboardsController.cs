0959fc37f62b1b9ece3da1cb04e6edeba58f5b5b
dogansalman <dogan.salman@yahoo.com>
2018-09-25 17:20:56 +0300

    update

--- Dashboard/Controllers/DashboardsController.cs
+++ Dashboard/Controllers/DashboardsController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using QuestionsSYS.ModelViews;
 using QuestionsSYS.Context;
 using System.Security.Claims;
+using System.Globalization;
 
 namespace QuestionsSYS.Controllers
 {
@@ -34,10 +35,12 @@ namespace QuestionsSYS.Controllers
             return View();
         }
        
-        public ActionResult Index()
+        public ActionResult Index(string date = null)
         {
+
             if (User.IsInRole("Admin"))
             {
+                
                 string totalQuestion = db.questions.ToList().Count().ToString();
                 string totalPersonnel = db.Users.ToList().Count().ToString();
                 string totalTasks = db.tasks.ToList().Count().ToString();
@@ -47,7 +50,8 @@ namespace QuestionsSYS.Controllers
                 ViewBag.totalPersonnel = totalPersonnel;
                 ViewBag.totalTasks = totalTasks;
                 ViewBag.ordertedTasks = ordered_tasks;
-
+                ViewBag.personnel = db.Users.ToList();
+              
             }
 
 
@@ -55,6 +59,72 @@ namespace QuestionsSYS.Controllers
             return View();
         }
 
+
+        public ActionResult Reports(string date = null)
+        {
+            if (!User.IsInRole("Admin")) return new HttpStatusCodeResult(403);
+
+            List<PersonnelReports> personnelList = new List<PersonnelReports>();
+            List<string> order_states = db.tasks.Select(t => t.order_state).Distinct().ToList();
+
+            if (date != null)
+            {
+                DateTime date1_ = DateTime.ParseExact(date.Split('-')[0].Trim(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                DateTime date2_ = DateTime.ParseExact(date.Split('-')[1].Trim(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                foreach (var item in db.Users.ToList())
+                {
+
+                    PersonnelReports person = new PersonnelReports
+                    {
+                        fullname = item.Name + " " + item.Surname,
+                        tasks = db.tasks.Where(t => t.user_id == item.Id && t.created_date > date1_ && t.created_date < date2_).Count(),
+                        states = new List<PersonnelOrderState>()
+                    };
+
+                    order_states.ForEach(stateName =>
+                    {
+                        var counts = db.tasks.Count(t => t.user_id == item.Id && t.order_state == stateName && t.created_date > date1_ && t.created_date < date2_);
+                        PersonnelOrderState pos = new PersonnelOrderState { state = stateName, count = counts };
+                        person.states.Add(pos);
+                    });
+
+                    personnelList.Add(person);
+                }
+
+                IEnumerable<PersonnelReports> model = new List<PersonnelReports>(personnelList);
+                return View(model);
+
+            }
+
+
+            
+            foreach (var item in db.Users.ToList())
+            {
+
+                PersonnelReports person = new PersonnelReports
+                {
+                    fullname = item.Name + " " + item.Surname,
+                    tasks = db.tasks.Where(t => t.user_id == item.Id).Count(),
+                    states = new List<PersonnelOrderState>()
+                };
+
+                order_states.ForEach(stateName =>
+                {
+                    var counts = db.tasks.Count(t => t.user_id == item.Id && t.order_state == stateName);
+                    PersonnelOrderState pos = new PersonnelOrderState { state = stateName, count = counts };
+                    person.states.Add(pos);
+                });
+
+                personnelList.Add(person);
+            }
+
+            IEnumerable<PersonnelReports> model_ = new List<PersonnelReports>(personnelList);
+            return View(model_);
+
+
+
+        }
         public ActionResult Dashboard_5()
         {
 
---
+++ Dashboard/ModelViews/PersonnelReports.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace QuestionsSYS.ModelViews
+{
+    public class PersonnelOrderState
+    {
+        private string state_;
+
+        public string state
+        {
+            get { return state_; }
+            set { state_ = value; }
+        }
+        private int count_;
+
+        public int count
+        {
+            get { return count_; }
+            set { count_ = value; }
+        }
+
+    }
+
+
+    public class PersonnelReports
+    {
+        private string fullname_;
+
+        public string fullname
+        {
+            get { return fullname_; }
+            set { fullname_ = value; }
+        }
+        private List<PersonnelOrderState> states_;
+
+        public List<PersonnelOrderState> states
+        {
+            get { return states_; }
+            set { states_ = value; }
+        }
+        private int tasks_;
+
+        public int tasks
+        {
+            get { return tasks_; }
+            set { tasks_ = value; }
+        }
+
+
+    }
+}
